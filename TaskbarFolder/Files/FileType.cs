b95b0aa639c525c4756400146ed9d7d2e8035778
mgrddsj <Jesse_Xu@live.com>
2021-03-21 15:06:44 +0800

    Add project files.

---
+++ TaskbarFolder/Files/FileType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskbarFolder.Files
+{
+    public enum FileType
+    {
+        File, Folder, Drive,
+        Shortcut
+    }
+}
---
+++ TaskbarFolder/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using TaskbarFolder.Explorer;
+using TaskbarFolder.Files;
+using TaskbarFolder.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskbarFolder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainViewModel Model
+        {
+            get => this.DataContext as MainViewModel;
+            set => this.DataContext = value;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Model.TryNavigateToPath(@"C:\Users\Jesse\Downloads");
+        }
+    }
+}
