edc64c0f85f3bfbc153ae98ee263738bcc64d82e
mgrddsj <Jesse_Xu@live.com>
2021-03-23 22:38:41 +0800

    Rationalize touchpad scrolling speed.

---
+++ TaskbarFolder/Helpers/Scroll.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TaskbarFolder
+{
+    static class Scroll
+    {
+        public static DependencyObject GetScrollViewer(this DependencyObject o)
+        {
+            // Return the DependencyObject if it is a ScrollViewer
+            if (o is ScrollViewer)
+            { return o; }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(o); i++)
+            {
+                var child = VisualTreeHelper.GetChild(o, i);
+
+                var result = GetScrollViewer(child);
+                if (result == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
+    }
+}
